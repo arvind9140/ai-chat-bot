6b15b2afe5f536e8fcea6f082e4c0cbd824981b4
Arvind Maurya <86925873+arvind9140@users.noreply.github.com>
2024-11-22 10:12:53 +0530

    Update index.py

--- index.py
+++ index.py
@@ -26,7 +26,7 @@ MONGODB_URI = os.getenv('MONGODB_URI')
 TOKEN = os.getenv('TOKEN')
 
 client = MongoClient(MONGODB_URI)
-db = client['uat_colonelz']
+db = client['interior_Design']
 project_collection = "project"
 lead_collection = "Lead"
 user_collection = "users"
